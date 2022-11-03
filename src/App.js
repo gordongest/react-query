cc6ad4f24277b43913a8947b2742b9aeee11e7b3
gordongest <>
2022-11-03 15:34:17 -0600

    add first query

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import { useQuery } from "@tanstack/react-query";
 import './App.css';
+
+const fetchUser = username => {
+  return fetch(`https://api.github.com/users/${username}`)
+      .then(res => res.json());
+}
+
+const GithubUser = ({ username }) => {
+  const userQuery = useQuery(
+      [username],
+      () => fetchUser(username)
+  )
+
+  const data = userQuery.data;
+
+  if (userQuery.isLoading) return <p>Loading...</p>
+  if (userQuery.isError) return <p>Error: {userQuery.error.message}</p>
+
+  return <pre>{JSON.stringify(data, null, 2)}</pre>
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <GithubUser username="gordongest" />
     </div>
   );
 }
